de3dc18f77c7332e3ef4293e6a87f1a17781bec0
Yulong Ran <yulong.ran@sjsu.edu>
2021-06-15 20:23:10 -0400

    Update Navbar styling to be responsive

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import SignInModal from "../components/SignInModal";
 import WelcomeModal from "../components/WelcomeModal";
-import Typography from "@material-ui/core/Typography";
 import Logo from "../assets/images/logo.svg";
 import "./Navbar.css";
 
@@ -26,37 +25,42 @@ function Navbar() {
   };
 
   return (
-    <div className="nav-container">
-      <div className="wrapper">
-        <nav>
-          <img src={Logo} alt="logo" className="nav-logo" />
-          <ul className="nav-items">
-            <li>
-              <Typography variant="body1" className="bold">
-                Companies Hiring
-              </Typography>
+    <nav className="navbar navbar-expand-lg navbar-light py-3">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <img src={Logo} alt="" height="38" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link h5 fw-bold">Companies Hiring</a>
             </li>
-            <li>
-              <Typography variant="body1" className="bold">
-                Resources
-              </Typography>
+            <li className="nav-item">
+              <a className="nav-link h5 fw-bold">Resources</a>
             </li>
-            <li>
-              <Typography variant="body1" onClick={handleSignInModalOpen}>
-                Login
-              </Typography>
+            <li onClick={handleSignInModalOpen} className="nav-item">
+              <a className="nav-link h5">Login</a>
             </li>
-            <li>
-              <Typography variant="body1" className="nav-outline-btn">
-                Sign up
-              </Typography>
+            <li className="nav-item">
+              <a className="nav-link nav-outline-btn">Sign up</a>
             </li>
           </ul>
-        </nav>
+        </div>
+        <SignInModal open={openSignInModal} handleClose={handleSignInModalClose} onSuccess={handleWelcomeModalOpen} />
+        <WelcomeModal open={openWelcomeModal} handleClose={handleWelcomeModalClose} />
       </div>
-      <SignInModal open={openSignInModal} handleClose={handleSignInModalClose} onSuccess={handleWelcomeModalOpen} />
-      <WelcomeModal open={openWelcomeModal} handleClose={handleWelcomeModalClose} />
-    </div>
+    </nav>
   );
 }
 
