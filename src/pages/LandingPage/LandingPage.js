daaf0c3da0a009862c9a7118fa2d13f6fb4e96f0
Yulong Ran <yulong.ran@sjsu.edu>
2021-06-18 18:44:47 -0400

    Updated landing page intro section styling

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -2,33 +2,31 @@ import React from "react";
 import "./LandingPage.css";
 import Navbar from "../../Navbar";
 import Footer from "../../Footer";
-import LandingImage from "../../assets/images/landing_page.svg";
+import LandingImage from "../../assets/images/landing_page.jpg";
 import Typography from "@material-ui/core/Typography";
 
 function LandingPage() {
   return (
-    <div>
+    <div className="landing-page">
       <Navbar />
-      <div className="landing-intro">
-        <div className="landing-intro-content">
-          <Typography variant="h1" className="title">
-            We are here for you!
-          </Typography>
-          <Typography variant="body2" className="body">
+      <div className="landing-intro my-5 container-lg">
+        <div className="container-lg">
+          <h1 className="title">We are here for you!</h1>
+          <h6 className="body">
             We show you companies now hiring and help you get to the next step in your career, specifically for Wall
             Street.
-          </Typography>
-          <div className="btn-group">
-            <Typography variant="body1" className="buyer-btn btn">
+          </h6>
+          <div className="mt-5 d-flex justify-content-between">
+            <button type="button" className="buyer-btn btn me-auto px-5 py-2">
               Sign up as a buyer
-            </Typography>
-            <Typography variant="body1" className="seller-btn btn">
+            </button>
+            <button type="button" className="seller-btn btn m-auto px-5 py-2">
               Sign up as a seller
-            </Typography>
+            </button>
           </div>
         </div>
-        <div className="landing-intro-image">
-          <img src={LandingImage} alt="" />
+        <div className="container-lg d-none d-lg-block">
+          <img src={LandingImage} className="img-fluid" alt="" />
         </div>
       </div>
       <div className="landing-hiw">
